ef6de06c2085e2ea4e3c61c14bc28bb939f265ab
helmik <skator.kim@gmail.com>
2023-03-05 03:50:21 -0600

    change end point

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://izdev73924.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://a1dqpoc4g1.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
