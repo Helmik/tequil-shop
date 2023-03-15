b62403ff8d4b174070dbe64c485d5dd005bfde41
helmik <skator.kim@gmail.com>
2023-03-14 18:32:42 -0600

    Activate import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://pknm8yiadj.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://gjf0okujn9.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
